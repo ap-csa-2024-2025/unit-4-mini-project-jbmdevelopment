4d6649c97b871b9fdeaedbbfd0c264c04b8b1a9e
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-12-05 18:48:25 +0000

    aaaa

--- Main.java
+++ Main.java
@@ -1,39 +1,30 @@
 import java.util.Scanner;
 
 public class Main
-{
+{ // use this to save this file for now
 	public static void main(String[] args)
 	{
-		// No need to touch anything in here
-		Scanner sc = new Scanner(System.in);
-		String str = null;
-
-		System.out.println("Enter a sentence:");
-		str = sc.nextLine();
-
-		algorithm1(str);
-		algorithm2(str);
-
-		sc.close();
+		playGame();
 	}
-
-	// This method should implement Algorithm 1 and print all neccessary data
-	public static void algorithm1(String input)
-	{
-		String fix = input.toLowerCase();
-		String empty = ""; // our valid string.
-		for(int i = 0; i < fix.length(); i++) {
-			String letter = fix.substring(i, i + 1);
-			// check index.
-			System.out.println(letter);
+	public int getPlayer2Move(int round) {
+		int result;
+		if(round % 3 == 0) {
+			result = 3;
+		} else if(round % 2 == 0) {
+			result = 2;
+		} else {
+			result = 1;
+		}
+		return result;
+	}
+	public static void playGame() {
+		int coins1 = 10; // player 1 
+		int coints2 = 10; // player 2
+		int currentRound = 1;
+		int maxRounds = 5;
+		boolean gameEnd = (currentRound >= maxRounds)// || (true);
+		while(!gameEnd) {
 			
 		}
-	}
-
-	// This method should implement Algorithm 2 and print all neccessary data
-	public static void algorithm2(String input)
-	{
-		// TODO: Put solution to Algorithm 1 here
-		System.out.println("Implement me!");
 	}
 }
